978b576fe5ea1c482905add872fc1e5fc73d8aaa
built-by-manas <thisismanas1512@gmail.com>
2025-12-09 16:33:42 +0530

    update timer display to HH:MM:SS format

--- main.go
+++ main.go
@@ -7,12 +7,17 @@ import (
 
 func main() {
 	fmt.Println("Pomodoro Timer Started...")
-	startTimer(13)
+	startTimer(3725)
 }
 
 func startTimer(seconds int) {
 	for i := seconds; i > 0; i-- {
-		fmt.Printf("\rTime Remaining: %d seconds... ", i)
+		h := i / 3600
+		m := (i % 3600) / 60
+		s := i % 60
+
+		fmt.Printf("\r⏳ Time Remaining: %02d:%02d:%02d   ", h, m, s)
+
 		time.Sleep(1 * time.Second)
 	}
 	fmt.Println("\nTimer Finished!")
