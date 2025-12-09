f42136ca07eba1d8ce51e4a82be44b5d280c200f
built-by-manas <thisismanas1512@gmail.com>
2025-12-09 16:22:22 +0530

    implement basic timer loop

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("Pomodoro Timer Started...")
+	startTimer(13)
+}
+
+func startTimer(seconds int) {
+	for i := seconds; i > 0; i-- {
+		fmt.Printf("\rTime Remaining: %d seconds... ", i)
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Println("\nTimer Finished!")
 }
